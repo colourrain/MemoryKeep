e4a672ce1c15153f33499d5eefc6e62bb891fad9
Bo Jiang <Bo@>
2018-05-15 03:43:33 +0800

    Initial Commit

---
+++ MemoryKeep/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MemoryKeep
+//
+//  Created by Bo Jiang on 2018/5/15.
+//  Copyright © 2018 com.bobo.MemoryKeep. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
